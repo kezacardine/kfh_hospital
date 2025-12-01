0e056e41f0f2a4dfd12994c941dbd3574f7df5d7
Celine Muhoracyeye <celine.muhoracyeyee@gmail.com>
2025-12-01 15:26:25 -0700

    Refactor and add tests for dashboard features
    Refactor tests to improve organization and add new tests for dashboard functionality.

--- hospital/tests.py
+++ hospital/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Patient, Department, VitalSign
 
 # Create your tests here.
 class SimpleTest(TestCase):
     def test_example(self):
         self.assertEqual(1 + 1, 2)
 
-  from django.test import TestCase
-from .models import Patient
 
 class PatientModelTest(TestCase):
     def test_create_patient(self):
@@ -20,26 +21,17 @@ class PatientModelTest(TestCase):
         self.assertEqual(patient.age, 25)
 
 
-from django.test import TestCase
-from .models import Department
-
 class DepartmentModelTest(TestCase):
     def test_department_creation(self):
         dept = Department.objects.create(name="Cardiology")
         self.assertEqual(dept.name, "Cardiology")
 
 
-
-from django.test import TestCase
-from django.urls import reverse
-
 class ViewTests(TestCase):
     def test_homepage_loads(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
-
-from django.contrib.auth.models import User
 
 class LoginTest(TestCase):
     def test_user_login(self):
@@ -53,4 +45,27 @@ class EmergencyTest(TestCase):
         response = self.client.get(reverse('emergency'))
         self.assertEqual(response.status_code, 200)
 
-  
+
+# Put the quick actions test inside a TestCase class
+class DashboardQuickActionsTest(TestCase):
+    def setUp(self):
+        self.client.login(username='admin', password='pass123')  # make sure admin exists or create user before
+
+    def test_quick_actions_links(self):
+        response = self.client.get(reverse('hospital:dashboard'))
+        self.assertContains(response, reverse('hospital:analytics'))
+        self.assertContains(response, reverse('admin:hospital_patient_add'))
+        self.assertContains(response, reverse('admin:hospital_vitalsign_add'))
+
+
+class DashboardContextTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='admin', password='pass123')
+        # Create sample patients
+        Patient.objects.create(first_name='John', last_name='Doe', gender='M', phone='123')
+        Patient.objects.create(first_name='Jane', last_name='Smith', gender='F', phone='456')
+        self.client.login(username='admin', password='pass123')
+    
+    def test_dashboard_counts(self):
+        response = self.client.get(reverse('hospital:dashboard'))
+        self.assertEqual(response.context['total_patients'], Patient.objects.count())
